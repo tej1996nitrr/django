bf8989986557ebaff66340d099890ee3d829e6c5
Tejas Kanikdaley <v-tekani@microsoft.com>
2019-08-12 20:58:51 +0530

    added static files

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,3 +4,4 @@ from django.http import HttpResponse
 def index(request):
     my_dict={'insert_me':"hello i am from moved views.py"}
     return render(request,'first_app/index.html',context=my_dict)
+
