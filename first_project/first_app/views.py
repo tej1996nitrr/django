1c10da844e9dea44555846f31d52bf02035996b7
Tejas Kanikdaley <v-tekani@microsoft.com>
2019-08-22 21:32:43 +0530

    added forms

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,AccessRecord,Webpage
+from . import forms
 # Create your views here.
 def index(request):
-    my_dict={'insert_me':"hello i am from moved views.py"}
-    return render(request,'first_app/index.html',context=my_dict)
-
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {"access_records": webpages_list}
+    #my_dict={'insert_me':"hello i am from moved views.py"}
+    return render(request,'first_app/index.html',date_dict)
+def form_name(request):
+    form = forms.Formname()
+    if request.method=="POST":
+        form = forms.Formname(request.POST)
+        if form.is_valid():
+            print("Validation success")
+            print("Name "+form.cleaned_data['name'])
+            print("Email "+form.cleaned_data['email'])
+            print("Text "+form.cleaned_data['text'])
+    return render(request,'first_app/form_page.html',{'form':form})
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -22,5 +22,7 @@ from first_app import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^first_app/',include('first_app.urls')),
+    #url(r'^admin/',admin.site.urls),
     path('admin/', admin.site.urls),
+    url(r'formpage/',views.form_name,name='form_name')
 ]
